6183e916f4ae51412142485a8ad2cf83c8ac1695
cannero <serunmal@gmail.com>
2024-04-28 22:12:39 +0200

    pong for each message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
+use std::io::{prelude::*, Error};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +10,35 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                write!(stream, "+PONG\r\n").unwrap();
+                thread::spawn(move ||{
+                    handle_connection(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+                //write!(stream, "+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
+    let mut buffer = [0; 1024];
+
+    loop {
+        // Read data
+        let n = stream.read(&mut buffer)?;
+
+        if n == 0 {
+            // Connection closed by client
+            println!("Connection closed by client");
+            return Ok(()); 
+        }
+
+        println!("Received from client: {}", String::from_utf8_lossy(&buffer[..n]));
+
+        write!(stream, "+PONG\r\n").unwrap();
     }
 }
