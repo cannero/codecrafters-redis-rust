e8ace6b0228217bc569cffca402d056f1e355b37
cannero <serunmal@gmail.com>
2024-05-26 21:28:13 +0200

    send replies to leader

--- src/replication_client.rs
+++ src/replication_client.rs
@@ -17,40 +17,42 @@ pub async fn start_replication(
 ) -> Result<()> {
     let mut stream = TcpStream::connect(leader_addr).await?;
 
-    send_command(Command::get_ping_command(), &mut stream).await?;
-    let reply = get_reply(&mut stream).await?;
+    send_message(Command::get_ping_command(), &mut stream).await?;
+    let reply = read_from_leader(&mut stream).await?;
     ReplicationHandler::check_ping_reply(&reply)?;
 
-    send_command(
+    send_message(
         Command::get_replconf_command("listening-port", listener_port),
         &mut stream,
     )
     .await?;
-    let reply = get_reply(&mut stream).await?;
+    let reply = read_from_leader(&mut stream).await?;
     ReplicationHandler::check_replconf_reply(&reply).context("listening port")?;
 
-    send_command(Command::get_replconf_command("capa", "psync2"), &mut stream).await?;
-    let reply = get_reply(&mut stream).await?;
+    send_message(Command::get_replconf_command("capa", "psync2"), &mut stream).await?;
+    let reply = read_from_leader(&mut stream).await?;
     ReplicationHandler::check_replconf_reply(&reply).context("capa")?;
 
-    send_command(Command::get_psync_command("?", -1), &mut stream).await?;
-    let reply = get_reply(&mut stream).await?;
+    send_message(Command::get_psync_command("?", -1), &mut stream).await?;
+    let reply = read_from_leader(&mut stream).await?;
     ReplicationHandler::check_psync_reply(&reply)?;
 
-    let _rdb_file = get_reply_raw(&mut stream).await?;
+    let _rdb_file = read_from_leader_raw(&mut stream).await?;
 
     loop {
-        let repl_message = get_reply(&mut stream).await?;
-        handler.handle(repl_message).await?;
+        let repl_message = read_from_leader(&mut stream).await?;
+        if let Some(reply) = handler.handle(repl_message).await? {
+            send_message(reply, &mut stream).await?;
+        }
     }
 }
 
-async fn send_command(command: Message, stream: &mut TcpStream) -> Result<()> {
-    stream.write_all(&command.to_data()).await?;
+async fn send_message(message: Message, stream: &mut TcpStream) -> Result<()> {
+    stream.write_all(&message.to_data()).await?;
     Ok(())
 }
 
-async fn get_reply_raw(stream: &mut TcpStream) -> Result<BytesMut> {
+async fn read_from_leader_raw(stream: &mut TcpStream) -> Result<BytesMut> {
     let mut buffer = BytesMut::with_capacity(1024);
     let n = stream.read_buf(&mut buffer).await?;
     if n == 0 {
@@ -60,8 +62,8 @@ async fn get_reply_raw(stream: &mut TcpStream) -> Result<BytesMut> {
     Ok(buffer.split())
 }
 
-async fn get_reply(stream: &mut TcpStream) -> Result<Message> {
-    let buffer = get_reply_raw(stream).await?;
+async fn read_from_leader(stream: &mut TcpStream) -> Result<Message> {
+    let buffer = read_from_leader_raw(stream).await?;
 
     Ok(parse_data(buffer)?)
 }
